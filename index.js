d32757462058b0c62d5154a660d4426b2bcd3c4a
Geng-Wung <gengxis92@gmail.com>
2022-08-29 13:24:07 -0400

    commit tree c04564f0f68e03223a12be4ca211c83c9ca16de0

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = (num1*num2);
+
+multiply;
+
+let random = Math.floor(Math.random() * 10)+1;
+
+random;
+
+let num3 = 14;
+let num4 = 5;
+
+let mod = (num3%num4);
+
+mod;
+
+let max = Math.max(1,20);
+
+max;
+
+
+
